44e6285487d5a13382c95092f28685f8ae4c0b3d
Xand3rdrag0n <alexkohr@gmail.com>
2025-07-07 12:51:04 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Example usage of the method
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+        System.out.println(findKthLargest(arr, k)); // Output: 4
+        System.out.println("Hello World!");
+    }
+}
